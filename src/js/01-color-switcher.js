12b0e10ceda6d2bd8873f0df2b24720bb0925181
Starovoy <staravoy@outlook.com>
2022-07-01 18:31:10 +0300

    homework fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+//---------------------variables----------------------------//
 
+const startBtn = document.querySelectorAll('[data-start]');
+const stopBtn = document.querySelectorAll('[data-stop]');
+let timerId = null;
+
+//---------------------function----------------------------//
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn[0].addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor= getRandomHexColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+});
